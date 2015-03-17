b6e5942f534352264a9ca4d1137e516f4b123ab5
Keiji <kimuk170@newschool.edu>
2015-03-16 22:37:34 -0400

    Setting up scenes...

---
+++ MidtermProject/Assets/rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	public float time = 50;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Rotate (0,0,time*Time.deltaTime); //rotates 50 degrees per second around z axis
+	}
+}
